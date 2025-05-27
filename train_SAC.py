1c533657e254817e87aef0426f6e6a4e1d0d131a
GuanH <r13922167@ntu.edu.tw>
2025-05-27 19:37:08 +0800

    add multiple reference broadcast and update train_SAC.py

--- train_SAC.py
+++ train_SAC.py
@@ -14,17 +14,18 @@ from tqdm import tqdm
 os.environ['SUPPRESS_MA_PROMPT']='1'
 
 # ==== Hyperparameters ====
-LR_ACTOR = 1e-4
-LR_CRITIC = 1e-4
+LR_ACTOR = 1e-5
+LR_CRITIC = 1e-5
 LR_ALPHA = 1e-5
 GAMMA = 0.9
 TAU = 0.01
 BATCH_SIZE = 1024
-BUFFER_SIZE = int(1e6)
+BUFFER_SIZE = int(3e4)
 EPISODES = 100000
-STEPS_PER_EPISODE = 100
+STEPS_PER_EPISODE = 30
 N_AGENTS = 3
-
+WARMUP_EP = 100
+COMM_DIM = 10
 # ==== Actor ====
 class Actor(nn.Module):
     def __init__(self, obs_dim, hid_dim, act_dim):
@@ -44,16 +45,6 @@ class Actor(nn.Module):
     def forward(self, x):
         x = self.fc(x)
         return self.h1(x), torch.clamp(self.h2(x), -20, 2).exp()
-
-class CommDecoder(nn.Module):
-    def __init__(self, comm_dim, hid_dim, obs_dim):
-        super().__init__()
-        self.fc = nn.Sequential(
-                nn.Linear(comm_dim, hid_dim), nn.SiLU(),
-                nn.Linear(hid_dim, hid_dim), nn.SiLU(),
-                nn.Linear(hid_dim, obs_dim)) 
-    def forward(self, comm):
-        return self.fc(comm)
 
 # ==== Critic ====
 class Critic(nn.Module):
@@ -77,15 +68,19 @@ class Critics(nn.Module):
         self.Q1 = Critic(obs_dim, hid_dim, act_dim)
         self.Q2 = Critic(obs_dim, hid_dim, act_dim)
 
-    # s(n_agents, B, obs), a(n_agents, B, act)
+    # s(n_agents, B, obs+comm), a(n_agents, B, act)
     def forward(self, s, a, idx):
         B = s.shape[1]
+        s = s[:,:,:-COMM_DIM]
+        a = a[:,:,:-COMM_DIM]
         # index 0 is the one it critics
         if idx != 0:
             s_c = s.clone()
             a_c = a.clone()
-            s_c[[0, idx]] = s[[idx, 0]]
-            a_c[[0, idx]] = a[[idx, 0]]
+            s_c[0] = s[idx]
+            s_c[idx] = s[0]
+            a_c[0] = a[idx]
+            a_c[idx] = a[0]
             s=s_c
             a=a_c
         # (B, n_agents * obs) <- (n_agents, B, obs)
@@ -113,7 +108,7 @@ class ReplayBuffer:
         self.state[self.pos] = state
         self.next_state[self.pos] = next_state
         self.action[self.pos] = action
-        self.reward[self.pos,:, 0] = reward
+        self.reward[self.pos] = reward[:, None]
         self.done[self.pos, 0] = done
         self.last_pos = self.pos
 
@@ -146,33 +141,24 @@ class MASAC:
                              hid_dim=512,
                              act_dim=act_dim).to(device)
 
-        self.decoder = CommDecoder(comm_dim=comm_dim,
-                hid_dim=512,
-                obs_dim=obs_dim).to(device)
-
-        self.critic = Critics(obs_dim=obs_dim * n_agents,
+        self.critic = Critics(obs_dim=(obs_dim-COMM_DIM) * n_agents,
                                 hid_dim=512,
-                                act_dim=act_dim * n_agents).to(device)
+                                act_dim=(act_dim-COMM_DIM) * n_agents).to(device)
 
-        self.target_critic = Critics(obs_dim=obs_dim * n_agents,
+        self.target_critic = Critics(obs_dim=(obs_dim-COMM_DIM) * n_agents,
                                        hid_dim=512,
-                                       act_dim=act_dim * n_agents).to(device)
+                                       act_dim=(act_dim-COMM_DIM) * n_agents).to(device)
 
-        # for i, (actor, critic) in enumerate(zip(self.actors, self.critics)):
-        #     actor_path = os.path.join('models', f"agent_{i}_actor.pth")
-        #     critic_path = os.path.join('models', f"agent_{i}_critic.pth")
-        #     actor.load_state_dict(torch.load(actor_path))
-        #     critic.load_state_dict(torch.load(critic_path))
+        # actor_path = os.path.join('models', f"agent_actor1.pth")
+        # critic_path = os.path.join('models', f"agent_critic1.pth")
+        # self.actor.load_state_dict(torch.load(actor_path))
+        # self.critic.load_state_dict(torch.load(critic_path))
 
         self.target_critic.load_state_dict(self.critic.state_dict())
 
         self.actor_optim = optim.Adam(self.actor.parameters(), lr=LR_ACTOR)
-        self.decoder_optim = optim.Adam(self.decoder.parameters(), lr=LR_ACTOR)
         self.critic_optim = optim.Adam(self.critic.parameters(), lr=LR_CRITIC)
 
-        self.alpha = 0.01
-        self.log_alpha = torch.tensor(np.log(self.alpha), dtype=torch.float32, requires_grad=True, device=self.device)
-        self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=LR_ALPHA)
         self.clip_norm = 1
 
 
@@ -188,8 +174,8 @@ class MASAC:
             # The first two are (x, y) \in [-1, 1]
             # The rest are say_0~say_9 \in [0, 1]
             noise = np.random.normal(size=action.shape) * noise_scale
-            action = np.clip((action+1)*0.5 + noise, -1, 1)
-            env_actions.append([action[:4], action[4:]])
+            action = np.clip(action + noise, -1, 1)
+            env_actions.append([action[:2], action[2:]])
             actions.append(action)
         return env_actions, np.array(actions)
 
@@ -211,7 +197,6 @@ class MASAC:
         total_critic_loss = 0
 
         next_actions_all = []
-        next_log_p_all = []
 
         for i in range(self.n_agents):
             next_obs_i = next_obs_all[i]
@@ -220,13 +205,10 @@ class MASAC:
                 dist = Normal(mu, sigma)
                 z = dist.sample()
                 next_actions = torch.tanh(z)
-                next_log_p = (dist.log_prob(z) - torch.log(1 - next_actions.pow(2) + 1e-6)).sum(-1, keepdim=True)
                 next_actions_all.append(next_actions)
-                next_log_p_all.append(next_log_p)
 
         # (n_agents, B, act)
         next_actions_all = torch.stack(next_actions_all, dim=0)
-        next_log_p_all = torch.stack(next_log_p_all, dim=0)
 
         for i in range(self.n_agents):
             next_obs_i = next_obs_all[i]
@@ -236,10 +218,10 @@ class MASAC:
             with torch.no_grad():
                 q1, q2 = self.target_critic(next_obs_all, next_actions_all, i)
                 next_q = torch.min(q1, q2)
-                next_q = next_q - self.alpha * next_log_p_all[i:i+1]
                 target_q = rewards_i + GAMMA * next_q * (1 - dones)
 
             pred_q1, pred_q2 = self.critic(obs_all, actions_all, i)
+
             delta1 = pred_q1 - target_q
             delta2 = pred_q2 - target_q
             loss_q1 = (delta1**2).mean()
@@ -257,42 +239,46 @@ class MASAC:
                 dist = Normal(mu, sigma)
                 z = dist.rsample()
                 sample_actions = torch.tanh(z)
-                log_p = (dist.log_prob(z) - torch.log(1 - sample_actions.pow(2) + 1e-6)).sum(-1, keepdim=True)
-
-                sample_actions = 0.5 * (sample_actions + 1)
                 if i != j:
                     sample_actions = sample_actions.detach()
-                    log_p = log_p.detach()
-                else:
-                    log_p_i = log_p
                 sample_actions_all.append(sample_actions)
             sample_actions_all = torch.stack(sample_actions_all, dim=0)
+
             q1, q2 = self.critic(obs_all, sample_actions_all, i)
             q = torch.min(q1, q2)
-            loss_pi = (self.alpha * log_p_i - q).mean()
+            comm_sample_actions_all = []
+            comm_next_obs_all = []
+            for j in range(self.n_agents):
+                comm_next_obs_all.append(torch.cat((next_obs_all[j,:,:-self.comm_dim], sample_actions_all[i,:,-self.comm_dim:]), dim=-1))
+            comm_next_obs_all = torch.stack(comm_next_obs_all, dim=0)
+
+            for j in range(self.n_agents):
+                mu, sigma = self.actor(comm_next_obs_all[j])
+                dist = Normal(mu, sigma)
+                z = dist.rsample()
+                sample_actions = torch.tanh(z)
+                noise = torch.normal(0,0.1,size=sample_actions.shape).to(self.device)
+                sample_actions = torch.clip(sample_actions + noise, -1, 1)
+                comm_sample_actions_all.append(sample_actions)
+
+            comm_sample_actions_all = torch.stack(comm_sample_actions_all, dim=0)
+            q_com = torch.zeros_like(q)
+            for j in range(self.n_agents):
+                q1, q2 = self.critic(comm_next_obs_all, comm_sample_actions_all, j)
+                q_com += torch.min(q1, q2)
+
             self.actor_optim.zero_grad()
-            self.decoder_optim.zero_grad()
+            loss_pi = (-q_com / self.n_agents).mean()
             loss_pi.backward()
             nn.utils.clip_grad_norm_(self.actor.parameters(), self.clip_norm)
-            total_actor_loss += loss_pi.item()
 
-            pred_obs = self.decoder(sample_actions_all[i].detach()[:, -self.comm_dim:])
-            err = (pred_obs - obs_all[i])**2
-            loss_decoder = err.shape[0]/((1/err).mean())
-            loss_decoder.backward()
             self.actor_optim.step()
-            self.decoder_optim.step()
-
-
-            self.alpha_optim.zero_grad()
-            loss_alpha = self.log_alpha * (-log_p_i.mean().item() + self.act_dim)
-            loss_alpha.backward()
-            self.alpha_optim.step()
-            self.alpha = np.exp(self.log_alpha.item())
+            total_actor_loss += loss_pi.item()
 
 
         for param, target_param in zip(self.critic.parameters(), self.target_critic.parameters()):
             target_param.data.copy_(TAU * param.data + (1 - TAU) * target_param.data)
+
         return total_actor_loss / self.n_agents, total_critic_loss / self.n_agents
 
     def save_models(self, save_dir='checkpoints'):
@@ -307,11 +293,11 @@ class MASAC:
 # ==== Training Loop ====
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    env = make_env("multiple_reference", n_agents=N_AGENTS)
+    env = make_env("multiple_reference_broadcast", n_agents=N_AGENTS)
     env.reset()
     obs_dims = env.observation_space[0].shape[0]
     act_dims = sum([sp.shape[0] for sp in env.action_space[0].spaces])
-    agent = MASAC(obs_dims, act_dims, 10, N_AGENTS, device)
+    agent = MASAC(obs_dims, act_dims, COMM_DIM, N_AGENTS, device)
     ep_actor_losses = []
     ep_critic_losses = []
 
@@ -324,6 +310,8 @@ def main():
         critic_losses = []
 
         total_reward = np.zeros(len(env.agents))
+        _, last_reward, _, _ = env.step([[np.zeros(2), np.zeros(10)] for _ in range(N_AGENTS)])
+        last_reward = np.array(last_reward)
         done = False
         step = 0
         while not done:
@@ -331,18 +319,21 @@ def main():
             next_obs, rewards, dones, info = env.step(env_actions)
             total_reward += rewards
             next_obs = np.array(next_obs)
-            rewards = 1 + np.clip(np.array(rewards), -10, 0) / 10
+            rewards = np.array(rewards)
             if step > STEPS_PER_EPISODE:
                 done = True
-            agent.buffer.add(obs, next_obs, actions, rewards, done)
-            actor_loss, critic_loss = agent.train()
-            actor_losses.append(actor_loss)
-            critic_losses.append(critic_loss)
+            agent.buffer.add(obs, next_obs, actions, rewards-last_reward, done)
+            last_reward = rewards
+            if episode > WARMUP_EP:
+                actor_loss, critic_loss = agent.train()
+                actor_losses.append(actor_loss)
+                critic_losses.append(critic_loss)
             obs = next_obs
             step += 1
-        returns.append(-np.sum(total_reward))
-        ep_actor_losses.append(np.mean(actor_losses))
-        ep_critic_losses.append(np.mean(critic_losses))
+        if episode > WARMUP_EP:
+            returns.append(np.sum(total_reward))
+            ep_actor_losses.append(np.mean(actor_losses))
+            ep_critic_losses.append(np.mean(critic_losses))
         pbar.set_description(f'reward:{sum(total_reward):.5f}')
         if episode % 10 == 0:
             tqdm.write(f"Episode {episode}: return {np.mean(returns[-100:]):.2f}, actor loss {np.mean(ep_actor_losses[-100:]):.4f}, critic loss {np.mean(ep_critic_losses[-100:]):.4f}")
@@ -360,8 +351,7 @@ def draw_result(returns, actor_losses, critic_losses):
     plt.subplot(1, 3, 1)
     plt.xlabel("Episode")
     plt.ylabel("Return")
-    plt.yscale("log")
-    plt.plot(episodes, returns, label='-log(reward)')
+    plt.plot(episodes, returns, label='reward')
     plt.title("Average Return")
     plt.grid(True)
 
@@ -369,7 +359,6 @@ def draw_result(returns, actor_losses, critic_losses):
     plt.subplot(1, 3, 2)
     plt.xlabel("Episode")
     plt.ylabel("Loss")
-    plt.yscale("linear")
     plt.plot(episodes, actor_losses, label='Actor Loss', color='orange')
     plt.title("Actor Loss")
     plt.grid(True)
@@ -378,7 +367,6 @@ def draw_result(returns, actor_losses, critic_losses):
     plt.subplot(1, 3, 3)
     plt.xlabel("Episode")
     plt.ylabel("Loss")
-    plt.yscale("linear")
     plt.plot(episodes, critic_losses, label='Critic Loss', color='green')
     plt.title("Critic Loss")
     plt.grid(True)
